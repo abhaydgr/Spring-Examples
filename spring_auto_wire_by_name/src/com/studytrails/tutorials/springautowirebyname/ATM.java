e7d6c6d004ecbe287ea3216ace523c43e1fce2d8
mithil <mithilcshah@gmail.com>
2016-03-17 17:09:54 +0530

    auto wire by name

---
+++ spring_auto_wire_by_name/src/com/studytrails/tutorials/springautowirebyname/ATM.java
@@ -0,0 +1,19 @@
+package com.studytrails.tutorials.springautowirebyname;
+
+public class ATM {
+
+	private Printer printer;
+
+	public Printer getPrinter() {
+		return printer;
+	}
+	public void setPrinter(Printer printer) {
+		this.printer = printer;
+	}
+
+	public void printBalanceInformation(String accountNumber) {
+		getPrinter().printBalanceInformation(accountNumber);
+
+	}
+
+}
