734c26bbe9e64477d9b23c7c039f36dfe94811eb
mithil <mithilcshah@gmail.com>
2016-03-17 17:15:48 +0530

    constructor injection

---
+++ spring_constructor_injection/src/com/studytrails/tutorials/springcontructorinjection/ATM.java
@@ -0,0 +1,17 @@
+package com.studytrails.tutorials.springcontructorinjection;
+
+public class ATM {
+
+	private Printer printer;
+
+	public ATM(Printer printer) {
+		this.printer = printer;
+	}
+
+	public void printBalanceInformation(String accountNumber) {
+		printer.printBalanceInformation(accountNumber);
+		
+	}
+	
+	
+}
