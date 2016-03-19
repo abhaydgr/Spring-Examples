ad345fd66808d8d67262c9cd10bf028bf0b69b61
mithil <mithilcshah@gmail.com>
2016-03-19 13:48:50 +0530

    Spring init

---
+++ spring_initializating_bean_disposable_bean/src/com/studytrails/tutorials/springinitializatingbeandisposablebean/ATM.java
@@ -0,0 +1,21 @@
+package com.studytrails.tutorials.springinitializatingbeandisposablebean;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class ATM implements InitializingBean, DisposableBean{
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("ATM's initialization method called. Connecting to bank's network.");
+		
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("ATM's destroy method called. Disconnecting from bank's network.");
+		
+	}
+	
+
+}
