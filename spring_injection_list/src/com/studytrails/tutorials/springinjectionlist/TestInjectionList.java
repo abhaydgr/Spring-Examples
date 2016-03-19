8792a661181b51fb3e095ec1717ed53538977318
mithil <mithilcshah@gmail.com>
2016-03-19 13:50:13 +0530

    Spring injection

---
+++ spring_injection_list/src/com/studytrails/tutorials/springinjectionlist/TestInjectionList.java
@@ -0,0 +1,17 @@
+package com.studytrails.tutorials.springinjectionlist;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestInjectionList {
+
+	public static void main(String[] args) {
+		System.out.println("************** BEGINNING PROGRAM **************");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+		ToDoList toDoList= (ToDoList)context.getBean("toDoList");
+		System.out.println("The contents of To Do List are : " + toDoList.getTasks());
+		
+		System.out.println("************** ENDING PROGRAM **************");
+	}
+}
