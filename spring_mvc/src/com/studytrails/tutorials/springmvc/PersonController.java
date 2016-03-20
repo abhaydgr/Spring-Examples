9877c63754cd4fda71cb343cc60791a26e4f894d
mithil <mithilcshah@gmail.com>
2016-03-20 12:36:33 +0530

    Spring mvc

---
+++ spring_mvc/src/com/studytrails/tutorials/springmvc/PersonController.java
@@ -0,0 +1,29 @@
+package com.studytrails.tutorials.springmvc;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PersonController {
+
+	private PersonService personService;
+
+	public PersonService getPersonService() {
+		return personService;
+	}
+
+	@Autowired
+	public void setPersonService(PersonService personService) {
+		this.personService = personService;
+	}
+
+	@RequestMapping("/allpersons")
+	public String showPersonListPage(Map<String, Object> model) {
+		model.put("persons", getPersonService().fetchAllPersons());
+		return "person_list";
+	}
+
+}
