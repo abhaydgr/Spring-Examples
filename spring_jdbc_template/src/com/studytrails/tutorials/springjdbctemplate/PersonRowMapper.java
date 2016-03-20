30adddd0ddc734bad5c52c0bdac1b70b60b7b45b
mithil <mithilcshah@gmail.com>
2016-03-20 11:59:29 +0530

    jdbc template

---
+++ spring_jdbc_template/src/com/studytrails/tutorials/springjdbctemplate/PersonRowMapper.java
@@ -0,0 +1,18 @@
+package com.studytrails.tutorials.springjdbctemplate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class PersonRowMapper implements RowMapper<Person>{
+
+	@Override
+	public Person mapRow(ResultSet resultSet, int rowNumber) throws SQLException {
+		Person person = new Person();
+	    person.setName(resultSet.getString(1));
+	    person.setEmail(resultSet.getString(2));
+	    return person;
+	}
+
+}
