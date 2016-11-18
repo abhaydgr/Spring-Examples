612fb6fb9338121751e9568c1baf859ad05b1f2e
mithil <mithil@bizansys>
2016-11-19 08:39:22 +1100

    Spring Setter Injection

--- spring-boot-basics/src/com/studytrails/springboot/basics/BasicsApplication.java
+++ spring-boot-basics/src/com/studytrails/springboot/basics/BasicsApplication.java
@@ -1,11 +1,19 @@
 package com.studytrails.springboot.basics;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class BasicsApplication {
+public class BasicsApplication implements CommandLineRunner {
 	public static void main(String[] args) {
-		SpringApplication.run(BasicsApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(BasicsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("main");
 	}
 }
